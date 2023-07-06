949007aca59818c107a5598bdf9f4aec0c3dac15
ANIKET PATRA <aniket.patra12345@gmail.com>
2023-07-06 17:20:11 +0530

    third commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-const a =6
+function getResult() {
+    let lowerbound = document.getElementById('lowerbound').value
+    let upperbound = document.getElementById('upperbound').value
+    let arr=[]
+    for(let i=lowerbound;i<=upperbound;i++){
+        if(isPrime(i)){
+            arr.push(i)
+        }
+    }
+    document.getElementById('result').innerHTML=arr
+}
+
+function isPrime(x) {
+    if (x === 0 || x === 1)
+        return false
+    for (let i = 2; i <= Math.sqrt(x); i++) {
+        if (x % i === 0) {
+            return false
+        }
+    }
+    return true
+}
